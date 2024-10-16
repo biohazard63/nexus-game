31bc9ad3d7a909d8b51c238f8b70cc2ce844bc8e
biohazard <114989783+biohazard63@users.noreply.github.com>
2024-10-16 10:15:14 +0200

    Update page.tsx

--- my-app/src/app/signup/page.tsx
+++ my-app/src/app/signup/page.tsx
@@ -2,7 +2,7 @@ import SignUpForm from "@/components/signup";
 
 export default function SignUpPage() {
     return (
-        <div className="flex items-center justify-center min-h-screen bg-gray-100">
+        <div className="flex items-center justify-center  ">
             <div className="w-full max-w-md">
                 <SignUpForm />
             </div>
